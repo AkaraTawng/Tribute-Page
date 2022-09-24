5d06f831a3501b3818b002ff273320e43ad93d1d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-09-24 08:19:31 +0700

    close menu when any nav link is clicked

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mobMenuBtn = document.querySelector("#mobile-menu-btn");
 const navBar = document.querySelector("#in-nav-links");
-const mobMenuBtnClose = document.querySelector("#mobile-menu-close")
+const mobMenuBtnClose = document.querySelector("#mobile-menu-close");
+const navItems = document.querySelectorAll(".link1");
 
 mobMenuBtn.addEventListener("click", () => {
     if(navBar.classList.contains("hide")){
@@ -15,3 +16,13 @@ mobMenuBtnClose.addEventListener("click", () => {
         navBar.classList.add("hide")
     }
 })
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        if(navBar.classList.contains("show")){
+            navBar.classList.remove("show")
+            navBar.classList.add("hide")
+        }
+    })
+})
+
