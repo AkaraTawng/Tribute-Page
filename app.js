acabfedc1f90f721814f00a45c664f929cc0a130
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-09-24 08:03:18 +0700

    open menu on mobMenuBtn click. close menu on MobMenuBtnClose click

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const mobMenuBtn = document.querySelector("#mobile-menu-btn");
 const navBar = document.querySelector("#in-nav-links");
+const mobMenuBtnClose = document.querySelector("#mobile-menu-close")
 
 mobMenuBtn.addEventListener("click", () => {
+    if(navBar.classList.contains("hide")){
+        navBar.classList.remove("hide")
+        navBar.classList.add("show")
+    }
+})
+
+mobMenuBtnClose.addEventListener("click", () => {
     if(navBar.classList.contains("show")){
         navBar.classList.remove("show")
         navBar.classList.add("hide")
-    } else if(navBar.classList.contains("hide")){
-        navBar.classList.remove("hide")
-        navBar.classList.add("show")
     }
 })
